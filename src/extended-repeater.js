1b69b8a09760cd87d0d814dca2046254124f886b
shchemer <gleb_2002@inbox.ru>
2023-04-16 15:05:48 +0300

    feat: complete extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -14,11 +14,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
+ *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, {repeatTimes = 0, separator = '+', addition = '', additionRepeatTimes = 0, additionSeparator = '|'} = {}) {
+  str = str + '';
+  addition = addition + ''
+  let newStr = '';
+  if (addition.length != 0) {
+    if (additionRepeatTimes != 0) {
+      for (let i = 0; i < additionRepeatTimes; i++) {
+        if (i == additionRepeatTimes - 1) {str += addition; continue;}
+        str += addition + additionSeparator;
+      }
+    }
+    else {
+      str += addition;
+    }
+  }
+  if (repeatTimes != 0) {
+    for (let i = 0; i < repeatTimes; i++) {
+      if (i == repeatTimes - 1) {newStr += str; continue;}
+      newStr += str + separator;
+    }
+  }
+  else {
+    return str;
+  }
+  return newStr;
 }
+
+console.log((repeater(9.234, { repeatTimes: 4, separator: '||', addition: { a: 5 }, additionRepeatTimes: 3, additionSeparator: '&&' })));
+console.log((repeater(-222, { repeatTimes: 4, separator: '||', addition: new Map(), additionRepeatTimes: 3, additionSeparator: '&&' })));
+console.log(((repeater(new Set(), { repeatTimes: 3, separator: '??? ', addition: [1, 2, 3, '4'], additionRepeatTimes: 2, additionSeparator: '!!!' }))));
+console.log(((repeater(true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' }))));
+console.log(((repeater(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' }))));
 
 module.exports = {
   repeater
