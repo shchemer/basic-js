1d48676bb7aaaa4b02718e1f1d509f2d9f5d4535
shchemer <gleb_2002@inbox.ru>
2023-04-17 22:09:25 +0300

    feat: complete task vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
@@ -20,13 +20,66 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(notReverse = true) {
+    this.notReverse = notReverse;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  alphabet() {
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numAlph = {};
+    for (let i = 0; i < alphabet.length; i++) {
+      numAlph[alphabet[i]] = i;
+    }
+    return [numAlph, alphabet];
+  }
+  turnOnMechanism(isEncrypt, text, key) {
+    let textCopy = text.split(' ');
+    text = text.toUpperCase().split(' ').join('');
+    key = key.toUpperCase().split(' ').join('');
+    let [numAlph, alphabet] = this.alphabet();
+    let encStr = '';
+    let indexEncChar;
+    let prevIndex = 0;
+    while(text.length != key.length) {
+      if (key.length > text.length) {key = key.slice(0, text.length); break};
+      key += key;
+    }
+    for (let i = 0; i < text.length; i++) {
+      if (numAlph[text[i]] === undefined) {encStr += text[i]; continue;};
+      if (isEncrypt) {
+        indexEncChar = (numAlph[text[i]] + numAlph[key[i]]) % 26;
+      }
+      else {
+        indexEncChar = (numAlph[text[i]] - numAlph[key[i]]) % 26;
+        indexEncChar = (indexEncChar < 0) ? (indexEncChar + 26) % 26 : indexEncChar;
+      }
+      encStr += alphabet[indexEncChar];
+    }
+    textCopy = textCopy.map((item, index) => {
+      let newItem;
+      if (index == 0) {
+        prevIndex += item.length;
+        newItem = encStr.slice(0, item.length);
+        return newItem;
+      }
+      else {
+        newItem = encStr.slice(prevIndex, prevIndex + item.length);
+        prevIndex += item.length;
+        return newItem;
+      }
+    })
+    textCopy = textCopy.join(' ');
+    if (!this.notReverse) {
+      textCopy = textCopy.split('').reverse().join('');
+    }
+    return textCopy;
+  }
+  encrypt(text, key) {
+    if (!text || !key) throw new Error('Incorrect arguments!');
+    return this.turnOnMechanism(true, text, key);
+  }
+  decrypt(text, key) {
+    if (!text || !key) throw new Error('Incorrect arguments!');
+    return this.turnOnMechanism(false, text, key);
   }
 }
 
